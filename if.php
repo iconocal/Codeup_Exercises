24eab244110e3a777816d8ec74b4d16a15adc363
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-20 15:38:58 -0600

    modified if exercise

--- if.php
+++ if.php
@@ -1,47 +1,10 @@
 <?php
 
-$a = 5;
-$b = 10;
-$c = '10';
+$wonLottery = false;
+$isRaining = true;
 
-if ($a < $b) {
-    // output the appropriate result
-    echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
-    // output the appropriate result
-    echo "$b is greater than $a\n";
-}
-
-if ($b >= $c) {
-    // output the appropriate result
-    echo "$b is greater than or equal to $c\n";
-}
-
-if ($b <= $c) {
-    // output the appropriate result
-    echo "$b is less than or equal to $c\n";
-}
-
-if ($b == $c) {
-    // output the appropriate result
-    echo "$b is equal to $c\n";
-}
-
-if ($b === $c) {
-    // output the appropriate result
-    echo "$b is identical to $c\n";
-}
-
-// output the appropriate result
-if ($b != $c) {
-    echo "$b is not equal to $c\n";
-}
-
-// output the appropriate result
-if ($b !== $c) {
-    echo "$b is not identical to $c\n";
+if ($wonLottery || $isRaining) {
+    echo "I'm buying a new car!\n";
 }
 
 ?>
