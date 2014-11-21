712b66eb48ec60a664f3c8e6e61edcafe3afb3d7
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-21 13:46:46 -0600

    These are my do while loop exercises

---
+++ do_while.php
@@ -0,0 +1,31 @@
+<?php
+
+// $a = 1;
+// do {
+//     echo "\$a is equal to {$a}\n";
+//     $a++;
+// } while ($a <= 5);
+
+// $count = 0;
+
+// do {
+// 	echo $count . PHP_EOL;
+// 	$count += 2;
+// }	while ($count <= 100);
+
+// $count = 100;
+
+// do {
+// 	echo $count . PHP_EOL;
+// 	$count -= 5;
+// }	while ($count >= -10);
+
+
+$a = 2;
+
+do {
+	echo $a . PHP_EOL;
+	$a = ($a * $a);
+} while ($a < 1000000);
+
+
