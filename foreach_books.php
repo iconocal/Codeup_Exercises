12576ea223c4e55858b98d169e2c141c871d91f0
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-25 14:25:46 -0600

    foreach_books with nested keys and values displayed

--- foreach_books.php
+++ foreach_books.php
@@ -1,2 +1,36 @@
 <?php
 
+// Construct a loop that iterates through each book and then each 
+// book's keys and values. Have it output the book's title, then list 
+// the key value pairs for the data about each book.
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+foreach ($books as $key => $book) {
+	echo "The book is $key\n";
+	foreach ($book as $key => $value) {
+		echo "$key equals {$value}\n";
+	}
+	echo "\n";
+}
