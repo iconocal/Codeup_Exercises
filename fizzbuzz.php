a3c8d6b9d25a32f259f90da6882d3332c91ebbed
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-25 11:10:28 -0600

    stored variable solution

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,7 +1,34 @@
 <?php
 
 
+// for ($num = 1; $num <= 100; $num++) {
+		
+// 	if($num % 3 == 0 && $num % 5 != 0) {
+// 			echo "Fizz\n";
+// 	} 
+
+// 	elseif($num % 5 == 0 && $num % 3 != 0) {
+// 		echo "Buzz\n";
+// 	}
+
+// 	elseif($num % 3 == 0 && $num % 5 == 0) {
+// 		echo "FizzBuzz\n";
+// 	}
+
+// 	else {
+// 		echo "\$num has a value of {$num}\n";
+// 	}
+
+// }
+
+// stored variable solution
+
 for ($num = 1; $num <= 100; $num++) {
+
+	$divBy3 = ($num % 3 == 0);
+	$divBy5 = ($num % 5 == 0);
+
+	// rest of code not modified to use $divBy variables
 		
 	if($num % 3 == 0 && $num % 5 != 0) {
 			echo "Fizz\n";
@@ -19,10 +46,4 @@ for ($num = 1; $num <= 100; $num++) {
 		echo "\$num has a value of {$num}\n";
 	}
 
-
-
-
-
 }
-
-
