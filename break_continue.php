d8a959e9b5c4b12a523a875ef8d378aba1410f3e
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-25 15:43:54 -0600

    added break in addition to continue

---
+++ break_continue.php
@@ -0,0 +1,26 @@
+<?php
+
+// Break Continue Exercise
+
+// CONTINUE
+
+// for ($i = 1; $i <= 100; $i++) {
+// 	if ($i % 2 != 0) {
+// 		continue;
+// 	}
+	
+// 	echo "$i\n";	
+// }
+
+// BREAK
+
+for ($i = 1; $i <= 100; $i++) {
+	echo "$i\n";
+	if ($i >= 10) {
+		break;
+	}
+}
+
+
+
+
