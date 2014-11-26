11411c97317fa227591ad3837261ef188c592b1b
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-26 15:20:32 -0600

    added divide by zero check

--- arithmetic.php
+++ arithmetic.php
@@ -1,59 +1,65 @@
 <?php
 
-function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo $a + $b . PHP_EOL;
-	} else {
-		echo "ERROR: Both arguments must be numbers\n";
-	}
-}
+// function add($a, $b) {
+// 	if (is_numeric($a) && is_numeric($b)) {
+//     	echo $a + $b . PHP_EOL;
+// 	} else {
+// 		echo "ERROR: Both arguments must be numbers\n";
+// 	}
+// }
 
-function subtract($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo $a - $b . PHP_EOL;
-	} else {
-		echo "ERROR: Both arguments must be numbers\n";
-	}
-}
+// function subtract($a, $b) {
+// 	if (is_numeric($a) && is_numeric($b)) {
+//     	echo $a - $b . PHP_EOL;
+// 	} else {
+// 		echo "ERROR: Both arguments must be numbers\n";
+// 	}
+// }
 
-function multiply($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo $a * $b . PHP_EOL;
-	} else {
-		echo "ERROR: Both arguments must be numbers\n";
-	}
+// function multiply($a, $b) {
+// 	if (is_numeric($a) && is_numeric($b)) {
+//     	echo $a * $b . PHP_EOL;
+// 	} else {
+// 		echo "ERROR: Both arguments must be numbers\n";
+// 	}
 
-}
+// }
 
 function divide($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
-    	echo $a / $b . PHP_EOL;
+    	if ($b === 0) {
+    		echo "ERROR: Divide By Zero!\n";
+		} else {
+			echo $a / $b . PHP_EOL;
+		} 
 	} else {
 		echo "ERROR: Both arguments must be numbers\n";
-	}
+		}	
 }
 
-function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a % $b . PHP_EOL;
-	} else {
-		echo "ERROR: Both arguments must be numbers\n";
-	}
-		
-}
+
+// function modulus($a, $b) {
+// 	if (is_numeric($a) && is_numeric($b)) {
+// 		echo $a % $b . PHP_EOL;
+// 	} else {
+// 		echo "ERROR: Both arguments must be numbers\n";
+// 	}		
+// }
+
+
 
 $num1 = 10;
 $num2 = 5;
 
-add($num1, $num2);
+// add($num1, $num2);
 
-subtract($num1, $num2);
+// subtract($num1, $num2);
 
-multiply($num1, $num2);
+// multiply($num1, $num2);
 
 divide($num1, $num2);
 
-modulus($num1, $num2);
+// modulus($num1, $num2);
 
 
 
