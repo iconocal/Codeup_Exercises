fde011641bb561c83bec6061275d336869800ff8
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-26 14:39:53 -0600

    arithmetic.php with notes and default values

--- arithmetic.php
+++ arithmetic.php
@@ -1,37 +1,81 @@
 <?php
 
-function add($a, $b) {
-    echo $a + $b;
-}
+// function add($a, $b) {
+//     echo $a + $b;
+// }
 
-function subtract($a, $b) {
-    echo $a - $b;
-}
+// function subtract($a, $b) {
+//     echo $a - $b;
+// }
 
-function multiply($a, $b) {
-    echo $a * $b;
-}
+// function multiply($a, $b) {
+//     echo $a * $b;
+// }
 
-function divide($a, $b) {
-    echo $a / $b;
-}
+// function divide($a, $b) {
+//     echo $a / $b;
+// }
 
-function modulus($a, $b) {
-	echo $a % $b;
-}
+// function modulus($a, $b) {
+// 	echo $a % $b;
+// }
 
-$num1 = 10;
-$num2 = 5;
+// $num1 = 10;
+// $num2 = 5;
 
-echo add($num1, $num2) . PHP_EOL;
+// echo add($num1, $num2) . PHP_EOL;
 
-echo subtract($num1, $num2) . PHP_EOL;
+// echo subtract($num1, $num2) . PHP_EOL;
 
-echo multiply($num1, $num2) . PHP_EOL;
+// echo multiply($num1, $num2) . PHP_EOL;
 
-echo divide($num1, $num2) . PHP_EOL;
+// echo divide($num1, $num2) . PHP_EOL;
 
-echo modulus($num1, $num2) . PHP_EOL;
+// echo modulus($num1, $num2) . PHP_EOL;
+
+
+// default value: boolean $strict determines which is done, if or else
+// this allows for different behaviors within the same function
+
+// function compare($a, $b, $strict = TRUE) {
+//     if ($strict === true) {
+//         echo $a === $b ? 'True' : 'False';
+//     } else {
+//         echo $a == $b ? 'True' : 'False';
+//     }
+
+//     echo PHP_EOL;
+// }
+
+// compare('Madonna', 'Lady Gaga', true);
+// compare('Madonna', 'Lady Gaga');
+
+
+// Validating Arguments
+// use variable casting to temporarily force the type of data you want
+// in this case it would convert input to integers if that's what we want
+//
+
+// function add($a, $b) {
+//     echo (int) $a +  (int) $b;
+// }
+
+// // But the BEST way to do this is like such:
+
+// function add($a, $b) {
+//     if (is_numeric($a) && is_numeric($b)) {
+//         echo $a + $b;
+//     } else {
+//         echo "ERROR: Both arguments must be numbers\n";
+//     }
+// }
+
+
+
+
+
+
+
 
 
 
