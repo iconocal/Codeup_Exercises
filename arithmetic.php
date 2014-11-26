7ef8b0356dca080520140fe934c470d626070a66
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-26 13:37:54 -0600

    User Defined Functions Exercise

---
+++ arithmetic.php
@@ -0,0 +1,28 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    echo $a - $b;
+}
+
+function multiply($a, $b) {
+    echo $a * $b;
+}
+
+function divide($a, $b) {
+    echo $a / $b;
+}
+
+echo add(1, 1) . PHP_EOL;
+
+echo subtract(10, 3) . PHP_EOL;
+
+echo multiply(3, 3) . PHP_EOL;
+
+echo divide(21, 3) . PHP_EOL;
+
+
+?>
