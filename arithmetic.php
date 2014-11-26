9967f0d3c4d43022b317b6b9341cbfe62350f43b
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-26 13:41:34 -0600

    added modulus function

--- arithmetic.php
+++ arithmetic.php
@@ -16,6 +16,10 @@ function divide($a, $b) {
     echo $a / $b;
 }
 
+function modulus($a, $b) {
+	echo $a % $b;
+}
+
 echo add(1, 1) . PHP_EOL;
 
 echo subtract(10, 3) . PHP_EOL;
@@ -23,6 +27,9 @@ echo subtract(10, 3) . PHP_EOL;
 echo multiply(3, 3) . PHP_EOL;
 
 echo divide(21, 3) . PHP_EOL;
+
+echo modulus(100, 2) . PHP_EOL;
+
 
 
 ?>
