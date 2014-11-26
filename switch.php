aa5f83f1a4c71329f21ac46d0eba42c0580841cf
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-11-26 10:28:32 -0600

    Switch Excercise Dates

--- switch.php
+++ switch.php
@@ -5,14 +5,31 @@
 
  // Get Day of Week as number
  // 1 (for Monday) through 7 (for Sunday)
+
  $dayOfWeek = date('N');
 
  switch($dayOfWeek) {
      case 1:
-         // Output Monday
+         echo "Monday\n";
+         break;
      case 2:
-         // Output Tuesday
-     // etc through day 7
+         echo "Tuesday\n";
+         break;
+     case 3:
+         echo "Wednesday\n";
+         break;
+     case 4:
+         echo "Thursday\n";
+         break;
+     case 5:
+         echo "Friday\n";
+         break;
+     case 6:
+         echo "Saturday\n";
+         break;
+     case 7:
+         echo "Sunday\n";
+         break;
  }
 
 
