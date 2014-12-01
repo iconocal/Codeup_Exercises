000f2cf5908fda53d7e13a867138996fd62f79ac
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-01 10:13:23 -0600

    internal functions exercise adding PHP empty() function

--- internal_functions.php
+++ internal_functions.php
@@ -10,6 +10,7 @@ $array = array(1, 2, 3);
 // Create a function that checks if a variable is set or 
 // empty, and display "$variable_name is SET|EMPTY"
 
+
 function checkVar($variable) {
 	if (isset($variable)) {
 		return "SET";
@@ -19,6 +20,14 @@ function checkVar($variable) {
 	
 }
 
+function checkEmpty($variable) {
+	if (empty($variable)) {
+		return "EMPTY";
+	} else {
+		return "NOT EMPTY";
+	}
+}
+
 echo "\$nothing is " . checkVar($nothing) . PHP_EOL;
 echo "\$something is " . checkVar($something) . PHP_EOL;
 echo "\$array is " . checkVar($array) . PHP_EOL;
@@ -26,18 +35,18 @@ echo "\$array is " . checkVar($array) . PHP_EOL;
 echo PHP_EOL;
 echo PHP_EOL;
 
+// Check to see is $something is EMPTY or not
+
+echo "\$something is " . checkEmpty($something) . PHP_EOL;
+
+echo PHP_EOL;
+echo PHP_EOL;
 
 
-
-
-
-// TEST: If var $nothing is set, display '$nothing is SET'
-
-// TEST: If var $nothing is empty, display '$nothing is EMPTY'
-
-// TEST: If var $something is set, display '$something is SET'
 
 // Serialize the array $array, and output the results
+
+
 
 // Unserialize the array $array, and output the results
 
