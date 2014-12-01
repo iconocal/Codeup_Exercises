ed924220291f3cd583bd674fbe764002400168a4
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-01 15:51:00 -0600

    Unit Conversion Challenge

---
+++ conversion.php
@@ -0,0 +1,24 @@
+<?php
+
+// Ask user for input measure unit (tsp), (tbsp), (floz), (cp), (pt).
+
+// Ask user for measurement: float type
+
+// Ask what unit measure to convert to (tsp), (tbsp), (floz), (cp), (pt).
+
+// probably be a switch statement, some functions:
+
+function convert_tsp_to_tbsp() {
+
+}
+
+// or
+
+function convert_to_tbsp($convert_from) {
+	switch($convert_from) {
+		case 'tsb':
+			formula
+			break;
+		case 
+	}
+}
