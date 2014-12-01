f393e0b1040156ca811eeb4c89c8ef79725f45fb
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-01 10:54:05 -0600

    updated with serialize unserialized

--- internal_functions.php
+++ internal_functions.php
@@ -11,15 +11,15 @@ $array = array(1, 2, 3);
 // empty, and display "$variable_name is SET|EMPTY"
 
 
-function checkVar($variable) {
-	unset($variable);
-	if (isset($variable)) {
-		return "SET";
-	} else {
-		return "EMPTY";
-	}
+// function checkVar($variable) {
+// 	unset($variable);
+// 	if (isset($variable)) {
+// 		return "SET";
+// 	} else {
+// 		return "EMPTY";
+// 	}
 	
-}
+// }
 
 
 // function checkEmpty($variable) {
@@ -31,12 +31,12 @@ function checkVar($variable) {
 // }
 
 
-echo "\$nothing is " . checkVar($nothing) . PHP_EOL;
-echo "\$something is " . checkVar($something) . PHP_EOL;
-echo "\$array is " . checkVar($array) . PHP_EOL;
+// echo "\$nothing is " . checkVar($nothing) . PHP_EOL;
+// echo "\$something is " . checkVar($something) . PHP_EOL;
+// echo "\$array is " . checkVar($array) . PHP_EOL;
 
-echo PHP_EOL;
-echo PHP_EOL;
+// echo PHP_EOL;
+// echo PHP_EOL;
 
 // Check to see is $something is EMPTY or not
 
@@ -48,11 +48,21 @@ echo PHP_EOL;
 
 
 // Serialize the array $array, and output the results
+$serializedArray = serialize($array);
+echo $serializedArray . PHP_EOL;
 
+echo PHP_EOL;
 
 
 // Unserialize the array $array, and output the results
 
+$unserial = unserialize($serializedArray);
+
+var_dump($unserial) . PHP_EOL;
+
+// echo unserialize($serializedArray) . PHP_EOL;
+
+// echo $unserial . PHP_EOL;
 
 
 
