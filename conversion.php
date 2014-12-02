ca49cefad6b1b50f1561e39fdf95cfd5892ef418
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-01 20:19:29 -0600

    Conversion Challenge with working getInput function

--- conversion.php
+++ conversion.php
@@ -1,6 +1,7 @@
 <?php
 
-// Ask user for input measure unit (tsp), (tbsp), (floz), (cp), (pt).
+// Ask user to choose input and output measures from array (tsp), (tbsp), (floz), (cp), (pt).
+// 
 
 // Ask user for measurement: float type
 
@@ -8,17 +9,59 @@
 
 // probably be a switch statement, some functions:
 
-function convert_tsp_to_tbsp() {
+$conFrom = array('[A]' => 'Tsp', '[B]' => 'Tbsp', '[C]' => 'fl oz', '[D]' => 'Cups', '[E]' => 'Pints', '[F]' =>'Quarts', '[G]' => 'Gallons');
 
+$tsp = 2.5;
+
+// Defined Functions
+
+// function listMeasures
+
+function listMeasure($measures) {
+	$measure = '';
+	foreach ($measures as $key => $value) {
+		// key++;
+		$measure .= "$key $value\n";
+	}
+	return "$measure\n";
 }
+
+// function getInput
+
+function getInput($numTrigger = false) {
+	if($numTrigger) {
+		return strtoupper(trim(fgets(STDIN)));
+	} else {
+		return round((trim(fgets(STDIN))), 2);
+	}
+}
+
+
+
+
+echo listMeasure($conFrom) . PHP_EOL;
+
+echo "Please select a unit of measure by letter: \n";
+
+$input = getInput($numTrigger = true);
+
+echo "You entered: $input\n";
+
+
+// function convert_tsp_to_tbsp($tsp) {
+// 	return "{$tsp} teaspoons = " . round(($tsp * 0.33333), 2) . " tablespoons\n";
+// }
+
+// echo convert_tsp_to_tbsp($tsp);
+
 
 // or
 
-function convert_to_tbsp($convert_from) {
-	switch($convert_from) {
-		case 'tsp':
-			formula
-			break;
-		case 
-	}
-}
+// function convert_to_tbsp($convert_from) {
+// 	switch($convert_from) {
+// 		case 'tsp':
+// 			formula
+// 			break;
+// 		case 
+// 	}
+// }
