058754046f5d1e460398ad29c590559cb3ab3cb5
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-02 14:30:52 -0600

    search_arrays exercise with working name search function

--- search_arrays.php
+++ search_arrays.php
@@ -6,11 +6,57 @@
  $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 
+////////////////////////
+// DEFINED FUNCTIONS //
+//////////////////////
+
+// Function: searchValues
+
+function searchforName($needle, $haystack) {
+	$lookFor = $needle;
+
+	$result = array_search($lookFor, $haystack);
+
+	if ($result === 0 || $result >= 1) {
+		// return "$haystack[$result]\n";
+		return 'TRUE';
+	} else {
+			return "FALSE";
+	}
+}
+
+
+// Function: compareValues
+
+
+// Program Begins Here
+
+$testName = 'Tina';
+
+echo searchforName($testName, $names) . PHP_EOL;
 
 
 
 
+// Thomas' Search for Name Function Code
+
+// $names1 = ['Wally', 'Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 
+// function foundValue($name, $array) {
+// 	$result = array_search($name, $array);
 
- 
+// 	// var_dump($result);
+
+// 	if ($result === false) {
+// 		return 'FALSE';
+// 	} else {
+// 			return 'TRUE';
+// 	}
+// }
+
+// $nameToSearch = 'Wally';
+// $arrayToSearch = $names1;
+
+// echo foundValue($nameToSearch, $arrayToSearch) . PHP_EOL;
+
