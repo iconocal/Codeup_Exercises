a56697fdac300c950a586cfeb866d6d9980b7649
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-03 11:30:17 -0600

    working list_with_commas version 1

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,21 +1,37 @@
 <?php
 
- // Converts array into list n1, n2, ..., and n3
- function humanizedList($array) {
-   // Your solution goes here!
- }
 
- // List of famous peeps
+// Converts array into list n1, n2, ..., and n3
+
+function humanized_list($inputArray) {
+	$lastName = array_pop($inputArray);
+	$newList = implode(', ', $inputArray);
+
+	return "{$newList}, and {$lastName}";
+
+}
+
+
+
+
+ // Original String to explode //
+
  $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
- // TODO: Convert the string into an array
- $physicistsArray = [];
 
- // Humanize that list
- $famousFakePhysicists = humanized_list($physicistsArray);
+// TODO: Convert the string into an array //
 
- // Output sentence
- echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
+$physicistsArray = explode(', ', $physicistsString);
+
+// Function call to humanize that list //
+
+$famousFakePhysicists = humanized_list($physicistsArray);
+
+// Output sentence //
+
+echo $famousFakePhysicists . PHP_EOL;
+
+
 
 
 
