587f127ddb3192f8e74fff641ebf7c37650dd8eb
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-03 10:25:24 -0600

    list with commas explode implode exercise

---
+++ list_with_commas.php
@@ -0,0 +1,27 @@
+<?php
+
+ // Converts array into list n1, n2, ..., and n3
+ function humanizedList($array) {
+   // Your solution goes here!
+ }
+
+ // List of famous peeps
+ $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+ // TODO: Convert the string into an array
+ $physicistsArray = [];
+
+ // Humanize that list
+ $famousFakePhysicists = humanized_list($physicistsArray);
+
+ // Output sentence
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
+
+
+
+
+
+
+
+
+ ?>
